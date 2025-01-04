6840dad12cd78621de17c47b3ccda2096a6595d8
IceWolf0110 <86553418+IceWolf0110@users.noreply.github.com>
2025-01-04 23:17:13 +0700

    upload

---
+++ installscript.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Define the lines you want to add
+line1="fastestmirror=True"
+line2="install_weak_deps=False"
+
+# Add the lines to the /etc/dnf/dnf.conf file if they don't already exist
+grep -qxF "$line1" /etc/dnf/dnf.conf || echo "$line1" >> /etc/dnf/dnf.conf
+grep -qxF "$line2" /etc/dnf/dnf.conf || echo "$line2" >> /etc/dnf/dnf.conf
+
+echo "dnf.conf configured"
+
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+sudo dnf update --refresh
+
+sudo dnf install akmod-nvidia xorg-x11-drv-nvidia-cuda xorg-x11-drv-nvidia-power git ffmpeg power-profiles-daemon NetworkManager-wifi fuse fuse-libs fastfetch pipewire-pulse @hardware-support
+
+echo "required packages installed"
+
+sudo dnf install gnome-shell file-roller firefox unzip unrar p7zip gnome-backgrounds nautilus gnome-terminal gnome-terminal-nautilus gnome-software gnome-control-center gnome-system-monitor
+echo "installed gnome"
+
+sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+echo "enabled Flathub"
+
+flatpak install ExtensionManager
+echo "installed Extension Manager" 
+
+sudo systemctl set-default graphical
+echo "enabled desktop mode"
+
