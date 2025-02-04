f1daa7b9bb06366b3827a49f136bd327c829968a
icewolf0110 <icewolf0110@gmail.com>
2025-02-04 22:02:23 +0700

    update script

--- installscript.sh
+++ installscript.sh
@@ -5,8 +5,8 @@ line1="fastestmirror=True"
 line2="install_weak_deps=False"
 
 # Add the lines to the /etc/dnf/dnf.conf file if they don't already exist
-grep -qxF "$line1" /etc/dnf/dnf.conf || echo "$line1" >> /etc/dnf/dnf.conf
-grep -qxF "$line2" /etc/dnf/dnf.conf || echo "$line2" >> /etc/dnf/dnf.conf
+grep -qxF "$line1" /etc/dnf/dnf.conf || sudo echo "$line1" >> /etc/dnf/dnf.conf
+grep -qxF "$line2" /etc/dnf/dnf.conf || sudo echo "$line2" >> /etc/dnf/dnf.conf
 
 echo "dnf.conf configured"
 
@@ -17,20 +17,14 @@ sudo dnf update --refresh
 
 sudo dnf install akmod-nvidia xorg-x11-drv-nvidia-cuda xorg-x11-drv-nvidia-power git ffmpeg power-profiles-daemon NetworkManager-wifi fuse fuse-libs fastfetch pipewire-pulse @hardware-support
 sudo akmods --force
-sudo dracut --regenerate-all --force
-
+sudo dracut --regenerate-all --force -v
 echo "required packages installed"
 
-sudo dnf install gnome-shell file-roller firefox unzip unrar p7zip gnome-backgrounds nautilus gnome-terminal gnome-terminal-nautilus gnome-software gnome-control-center gnome-system-monitor
+sudo dnf install gnome-shell gnome-tweaks file-roller firefox unzip unrar p7zip gnome-backgrounds nautilus gnome-terminal gnome-terminal-nautilus gnome-software gnome-control-center gnome-system-monitor
 echo "installed gnome"
 
 sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 echo "enabled Flathub"
 
-flatpak install ExtensionManager
-echo "installed Extension Manager" 
-
 sudo systemctl set-default graphical
 echo "enabled desktop mode"
-
-reboot
